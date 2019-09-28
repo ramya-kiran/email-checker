c98e7e23dcefeb2c0bbedbb4624003f73381f7a1
Ramya Rao <ramrao@umail.iu.edu>
2019-09-28 18:31:06 -0400

    fixed an error with checking domain name

--- email_checker.py
+++ email_checker.py
@@ -29,16 +29,17 @@ def unique_email():
 
     # caching known domain names to reduce time spent in lookup
     known_email_dns = {'gmail.com': 0, 'yahoo.com': 0, 'hotmail.com': 0}
-    
+
     for e in email_list:
-        if isinstance(e, str):
+        if isinstance(e, str) and e not in unique_mails:
             e = e.lower()
             first, second, is_valid = check_domain(e, known_email_dns) # verifying domain name of the email addrss
             if is_valid:
                 user_valid, modified_username = check_username(first) # Verifying username
-                modified_addr = modified_username + '@' + second
-                if user_valid and modified_addr not in unique_mails:
-                    unique_mails[modified_addr] = 0
+                if user_valid:
+                    modified_addr = modified_username + '@' + second
+                    if modified_addr not in unique_mails:
+                        unique_mails[modified_addr] = 0
 
     ret = {
         'number_emails_input' : str(len(email_list)),
