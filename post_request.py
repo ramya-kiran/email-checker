0c8edde86ffa57b5f71cc613ff5ebe12b72ec25e
Ramya Rao <ramrao@umail.iu.edu>
2019-09-28 16:19:42 -0400

    final changes

--- post_request.py
+++ post_request.py
@@ -4,6 +4,7 @@ import requests
 "Citation: Code from stack overflow : https://stackoverflow.com/questions/10313001/is-it-possible-to-make-post-request-in-flask"
 
 dictToSend = {'email_list':["test.email+spam@gmail.com", "testemail@gmail.com", "test.email@gmail.com", "ramya_12@gmail.com"]}
+# ******* change this localhost url if necessary while testing ***************
 res = requests.post('http://localhost:5000/unique_email', json=dictToSend)
 print('response from server:', res.text)
 dictFromServer = res.json()
